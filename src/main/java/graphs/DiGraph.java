b67fc72ea7fce2518273bd8416d3c661650abe71
aleksandr_zheludkov <aleksandr_zheludkov@epam.com>
2017-12-07 14:41:03 +0300

    Little optimizing.

--- src/main/java/graphs/DiGraph.java
+++ src/main/java/graphs/DiGraph.java
@@ -39,6 +39,7 @@ public class DiGraph {
         if (source.children.contains(destination)) return true;
         Boolean result = false;
         for (Node child : source.children) {
+            if (result == true) return true;
             if (!isVisited.get(child)) result |= search(child, destination, isVisited);
         }
         return result;
